31cb8ca46c945ddb496e2705059022505ce122ce
Anvar Ramazanov <anvar.ramazanov@jetbrains.com>
2024-12-01 10:04:42 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,53 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var leftArr: MutableList<Int> = mutableListOf()
+        var rightArr: MutableList<Int> = mutableListOf()
+        for (i in input.indices) {
+            val tokens = input.getOrNull(i)?.split("   ")!!
+            leftArr.add(tokens[0].toInt())
+            rightArr.add(tokens[1].toInt())
+        }
+
+        leftArr.sort()
+        rightArr.sort()
+
+        var sum = 0
+        for (i in leftArr.indices) {
+            sum += abs(leftArr[i] - rightArr[i])
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var leftArr: MutableList<Int> = mutableListOf()
+        var rightMap: MutableMap<Int, Int> = mutableMapOf()
+        for (i in input.indices) {
+            val tokens = input.getOrNull(i)?.split("   ")!!
+            leftArr.add(tokens[0].toInt())
+            var right = tokens[1].toInt()
+
+            if (rightMap.containsKey(right)) {
+                rightMap.put(right, rightMap[right]!! + 1)
+            } else {
+                rightMap.put(right, 1)
+            }
+        }
+
+        var similarity = 0
+        for (left in leftArr) {
+            similarity += left * if (rightMap.containsKey(left)) rightMap.getValue(left) else 0
+        }
+
+        return similarity
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
