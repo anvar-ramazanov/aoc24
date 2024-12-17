3350548f4a7f13e79243393be7892c68e9a54604
Anvar Ramazanov <ramazanov.anvar@gmail.com>
2024-12-17 09:48:42 +0100

    Day 16 part 2

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,5 +1,11 @@
 fun main() {
-    data class State (val maze:  Map<Pair<Int, Int>, Char>, val startPos: Pair<Int, Int>, val endPos: Pair<Int, Int>, val maxRow: Int, val maxCol: Int )
+    data class State(
+        val maze: Map<Pair<Int, Int>, Char>,
+        val startPos: Pair<Int, Int>,
+        val endPos: Pair<Int, Int>,
+        val maxRow: Int,
+        val maxCol: Int
+    )
 
     fun parseMaze(input: List<String>): State {
         var startPos = Pair(0, 0)
@@ -48,7 +54,7 @@ fun main() {
         while (queue.isNotEmpty()) {
             val (currentPosition, currentDirection, currentCost) = queue.removeFirst()
 
-            if (costMap.getValue(currentPosition) < currentCost){
+            if (costMap.getValue(currentPosition) < currentCost) {
                 continue
             }
 
@@ -57,21 +63,28 @@ fun main() {
             if (currentPosition == state.endPos) continue
 
             // forward
-            val forwardPosition = Pair(currentPosition.first + currentDirection.first, currentPosition.second + currentDirection.second)
+            val forwardPosition =
+                Pair(currentPosition.first + currentDirection.first, currentPosition.second + currentDirection.second)
             if (state.maze[forwardPosition] != '#') {
                 queue.add(Move(forwardPosition, currentDirection, currentCost + 1))
             }
 
             // clockwise
             val clockwiseDirection = clockwise(currentDirection)
-            val clockwisePosition = Pair(currentPosition.first + clockwiseDirection.first, currentPosition.second + clockwiseDirection.second)
+            val clockwisePosition = Pair(
+                currentPosition.first + clockwiseDirection.first,
+                currentPosition.second + clockwiseDirection.second
+            )
             if (state.maze[clockwisePosition] != '#') {
                 queue.add(Move(clockwisePosition, clockwiseDirection, currentCost + 1001))
             }
 
             // counterclockwise
             val counterClockwiseDirection = counterClockwise(currentDirection)
-            val counterClockwisePosition = Pair(currentPosition.first + counterClockwiseDirection.first, currentPosition.second + counterClockwiseDirection.second)
+            val counterClockwisePosition = Pair(
+                currentPosition.first + counterClockwiseDirection.first,
+                currentPosition.second + counterClockwiseDirection.second
+            )
             if (state.maze[counterClockwisePosition] != '#') {
                 queue.add(Move(counterClockwisePosition, counterClockwiseDirection, currentCost + 1001))
             }
@@ -90,67 +103,120 @@ fun main() {
         return result
     }
 
-    fun calcPaths(state: State, minCost: Int) : Int {
-        val start = state.startPos
-        val end = state.endPos
-        val paths = mutableListOf<List<Pair<Int, Int>>>()
+    data class MapKey(val pos: Pair<Int, Int>, val direction: Pair<Int, Int>)
 
-        fun dfs(currentPosition: Pair<Int, Int>, currentDirection: Pair<Int, Int>, currentCost: Int, currentPath: MutableSet<Pair<Int, Int>>, paths: MutableList<List<Pair<Int, Int>>>) {
-            if (currentPosition == end) {
-                paths.add(currentPath.toList())
-                return
+    data class Move2(val position: Pair<Int, Int>, val direction: Pair<Int, Int>, val cost: Int, val prev: MapKey)
+
+    fun calcMinCostsMap2(
+        state: State,
+        costMap: MutableMap<MapKey, Int>,
+        trackMap: MutableMap<MapKey, MutableSet<MapKey>>
+    ) {
+        val queue = ArrayDeque<Move2>()
+        queue.add(Move2(state.startPos, Pair(0, 1), 0, MapKey(state.startPos, Pair(0, 1))))
+
+        while (queue.isNotEmpty()) {
+            val (currentPosition, currentDirection, currentCost, prevMove) = queue.removeFirst()
+
+            if (costMap.getValue(MapKey(currentPosition, currentDirection)) < currentCost) {
+                continue
+            }
+            if (costMap.getValue(MapKey(currentPosition, currentDirection)) == currentCost) {
+                trackMap.getValue(MapKey(currentPosition, currentDirection)).add(prevMove)
+            } else {
+                trackMap[MapKey(currentPosition, currentDirection)] = mutableSetOf(prevMove)
             }
 
-            if (currentCost >= minCost) return
+            costMap[MapKey(currentPosition, currentDirection)] = currentCost
 
-            if (currentPath.contains(currentPosition)) return
-
-            currentPath.add(currentPosition)
+            if (currentPosition == state.endPos) continue
 
             // forward
-            val forwardPosition = Pair(currentPosition.first + currentDirection.first, currentPosition.second + currentDirection.second)
+            val forwardPosition =
+                Pair(currentPosition.first + currentDirection.first, currentPosition.second + currentDirection.second)
             if (state.maze[forwardPosition] != '#') {
-                dfs(forwardPosition, currentDirection, currentCost + 1, currentPath.toMutableSet(), paths)
+                queue.add(
+                    Move2(
+                        forwardPosition,
+                        currentDirection,
+                        currentCost + 1,
+                        MapKey(currentPosition, currentDirection)
+                    )
+                )
             }
 
             // clockwise
             val clockwiseDirection = clockwise(currentDirection)
-            val clockwisePosition = Pair(currentPosition.first + clockwiseDirection.first, currentPosition.second + clockwiseDirection.second)
+            val clockwisePosition = Pair(
+                currentPosition.first + clockwiseDirection.first,
+                currentPosition.second + clockwiseDirection.second
+            )
             if (state.maze[clockwisePosition] != '#') {
-                dfs(clockwisePosition, clockwiseDirection, currentCost + 1001, currentPath.toMutableSet(), paths)
+                queue.add(
+                    Move2(
+                        clockwisePosition,
+                        clockwiseDirection,
+                        currentCost + 1001,
+                        MapKey(currentPosition, currentDirection)
+                    )
+                )
             }
 
             // counterclockwise
             val counterClockwiseDirection = counterClockwise(currentDirection)
-            val counterClockwisePosition = Pair(currentPosition.first + counterClockwiseDirection.first, currentPosition.second + counterClockwiseDirection.second)
+            val counterClockwisePosition = Pair(
+                currentPosition.first + counterClockwiseDirection.first,
+                currentPosition.second + counterClockwiseDirection.second
+            )
             if (state.maze[counterClockwisePosition] != '#') {
-                dfs(counterClockwisePosition, counterClockwiseDirection, currentCost + 1001, currentPath.toMutableSet(), paths)
+                queue.add(
+                    Move2(
+                        counterClockwisePosition,
+                        counterClockwiseDirection,
+                        currentCost + 1001,
+                        MapKey(currentPosition, currentDirection)
+                    )
+                )
             }
         }
-
-        dfs(start, Pair(0, 1), 0, mutableSetOf(), paths)
-
-        val uniqueTails = mutableSetOf<Pair<Int, Int>>()
-        for (path in paths) {
-            for (p in path) {
-                uniqueTails.add(p)
-            }
-        }
-
-        return uniqueTails.size + 1
     }
 
 
     fun part2(input: List<String>): Int {
         val state = parseMaze(input)
 
-        val costMap = mutableMapOf<Pair<Int, Int>, Int>().withDefault { Int.MAX_VALUE }
+        val costMap = mutableMapOf<MapKey, Int>().withDefault { Int.MAX_VALUE }
+        val trackMap = mutableMapOf<MapKey, MutableSet<MapKey>>()
 
-        calcMinCostsMap(state, costMap)
+        calcMinCostsMap2(state, costMap, trackMap)
 
-        val minCost = costMap.getValue(state.endPos)
+        val finish = listOf(
+            Pair(Pair(0, 1), costMap.getValue(MapKey(state.endPos, Pair(0, 1)))),
+            Pair(Pair(0, -1), costMap.getValue(MapKey(state.endPos, Pair(0, -1)))),
+            Pair(Pair(1, 0), costMap.getValue(MapKey(state.endPos, Pair(1, 0)))),
+            Pair(Pair(-1, 0), costMap.getValue(MapKey(state.endPos, Pair(-1, 0))))
+        )
 
-        return 0
+        val minCostDirection = finish.minBy { it.second }.first
+
+        val current = Pair(state.endPos, minCostDirection)
+        val visited = mutableSetOf<Pair<Int, Int>>()
+
+        fun dfs(current: Pair<Pair<Int, Int>, Pair<Int, Int>>) {
+            val (currentPosition, currentDirection) = current
+            visited.add(currentPosition)
+            if (currentPosition == state.startPos) return
+            val prevPositions = trackMap[MapKey(currentPosition, currentDirection)]!!
+            for (prevPosition in prevPositions) {
+                dfs(Pair(prevPosition.pos, prevPosition.direction))
+            }
+
+        }
+
+        dfs(current)
+
+
+        return visited.size
     }
 
     val testInput = readInput("Day16_test")
