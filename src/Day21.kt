15bfae80fdc758903fd2147461e854b79dd46700
Anvar Ramazanov <ramazanov.anvar@gmail.com>
2024-12-27 19:50:41 +0100

    Day 21 part 1

---
+++ src/Day21.kt
@@ -0,0 +1,173 @@
+import java.util.LinkedList
+import kotlin.math.abs
+
+fun main() {
+    data class Point(val point: Pair<Int, Int>, val path: String)
+
+    fun getNumericPosition(code: Char): Pair<Int, Int> {
+        return when (code) {
+            '7' -> return Pair(0, 0)
+            '8' -> return Pair(0, 1)
+            '9' -> return Pair(0, 2)
+            '4' -> return Pair(1, 0)
+            '5' -> return Pair(1, 1)
+            '6' -> return Pair(1, 2)
+            '1' -> return Pair(2, 0)
+            '2' -> return Pair(2, 1)
+            '3' -> return Pair(2, 2)
+            '0' -> return Pair(3, 1)
+            'A' -> return Pair(3, 2)
+             else -> {
+                 throw Exception("Invalid code")
+             }
+        }
+    }
+
+    fun getDirectionalPosition(code: Char): Pair<Int, Int> {
+        return when (code) {
+            '^' -> return Pair(0, 1)
+            'A' -> return Pair(0, 2)
+            '<' -> return Pair(1, 0)
+            'v' -> return Pair(1, 1)
+            '>' -> return Pair(1, 2)
+            else -> {
+                throw Exception("Invalid code")
+            }
+        }
+    }
+
+    fun findShortestPath(
+        from: Pair<Int, Int>,
+        to: Pair<Int, Int>,
+        skip: Pair<Int, Int>,
+        width: Int,
+        height: Int
+    ): List<String> {
+        val directions = listOf(
+            Pair(-1, 0) to '^',
+            Pair(1, 0) to 'v',
+            Pair(0, -1) to '<',
+            Pair(0, 1) to '>'
+        )
+        val queue = LinkedList<Point>()
+        var shortestPaths = mutableListOf<String>()
+
+        queue.add(Point(from, ""))
+        var shortestPathLength = abs(from.first - to.first) + abs(from.second - to.second)
+
+        while (queue.isNotEmpty()) {
+            val (current, currentPath) = queue.poll()
+
+            if (current.first == to.first && current.second == to.second) {
+                if (currentPath.length == shortestPathLength) {
+                    shortestPaths.add(currentPath + "A")
+                }
+                continue
+            }
+
+            if (currentPath.length >= shortestPathLength) continue
+
+            for ((direction, symbol) in directions) {
+                val newY = current.first + direction.first
+                val newX = current.second + direction.second
+
+                if (newX in 0 until width && newY in 0 until height && !(newX == skip.second && newY == skip.first)) {
+                    queue.add(Point(Pair(newY, newX), currentPath + symbol))
+                }
+            }
+        }
+
+        return shortestPaths
+    }
+
+    fun getTransformedLengthOnDepth(path: String, currentDepth: Int, maxDepth: Int): Int {
+        var prev = getDirectionalPosition('A')
+        var result = 0
+
+        for (c in path) {
+            val curr = getDirectionalPosition(c)
+
+            val paths = findShortestPath(prev, curr, Pair(0, 0), 3, 2)
+            var min = Integer.MAX_VALUE
+
+            if (currentDepth < maxDepth) {
+                for (path in paths) {
+                    val pathLength = getTransformedLengthOnDepth(path, currentDepth + 1, maxDepth)
+                    if (pathLength < min) {
+                        min = pathLength
+                    }
+                }
+            } else if (currentDepth == maxDepth) {
+                min = paths.minOf { it.length }
+            }
+            result += min
+            prev = curr
+        }
+
+        return result
+    }
+
+    fun getCodeLength(code: String, maxDepth: Int): Int {
+        var prev = getNumericPosition('A')
+        var result = 0
+
+        for (s in code) {
+            val curr = getNumericPosition(s)
+            val numericPaths = findShortestPath(prev, curr, Pair(3, 0), 3, 4)
+
+            var min = Integer.MAX_VALUE
+            for (path in numericPaths) {
+                val pp = getTransformedLengthOnDepth(path, 1, maxDepth)
+                if (pp < min) {
+                    min = pp
+                }
+            }
+
+            result += min
+            prev = curr
+        }
+
+        return result
+    }
+
+    fun getNumberFromCode(code: String): Int {
+        val sb = StringBuilder()
+        for (c in code) {
+            if (c.isDigit()) {
+                sb.append(c)
+            }
+        }
+        return sb.toString().toInt()
+    }
+
+    fun part1(input: List<String>): Int {
+        var result = 0
+        val codes = input.map { it.trim() }.toList()
+        for (code in codes) {
+            val length = getCodeLength(code, 2)
+            result += (length * getNumberFromCode(code))
+        }
+
+        return result
+    }
+
+    fun part2(input: List<String>): Int {
+        var result = 0
+        val codes = input.map { it.trim() }.toList()
+        for (code in codes) {
+            val length = getCodeLength(code, 10)
+            result += (length * getNumberFromCode(code))
+        }
+
+        return result
+    }
+
+    val testInput = readInput("Day21_test")
+    check(part1(testInput) == 126384)
+    //check(part2(testInput) == 0)
+
+
+    val input = readInput("Day21")
+    part1(input).println()
+    part2(input).println()
+}
